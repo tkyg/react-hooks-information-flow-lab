53a6de08e133561ed9d99315e93a10f4bdcbd057
tkyg <80114433+tkyg@users.noreply.github.com>
2022-06-01 13:27:21 -0400

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({onDarkModeClick, mode}){
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+          {mode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  )
+  }
+  export default Header
